9abe42b6bed03d2bac320bcd9c9bd8c5ee01a4ec
Paulo Oyama <papaloyama@gmail.com>
2025-06-12 09:10:32 -0300

    refactoring

--- bert/main.py
+++ bert/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:539f934b7ebe1ea273b9550ba7931e823651c61ca8ada908db70f7aafb09f560
-size 1790
+oid sha256:42e35f0b075b8e710d2e3a929da1d5a462dc84588c34d0b769ea26b2b4fb60e1
+size 4294
